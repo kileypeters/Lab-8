ccaa8ff3bdca2c7d38a9f5f075770446bc6b9c8c
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-26 13:57:20 -0500

    Added javadoc

--- src/Shape.java
+++ src/Shape.java
@@ -1,7 +1,17 @@
-
+/**
+ * Class representing the shapes of game pieces.
+ * 
+ * @author Kiley
+ *
+ */
 public enum Shape {
 	BOOT, RACECAR, THIMBLE,;
 	
+	/**
+	 * Gives the name of the value of the enum object, in lowercase.
+	 * 
+	 * @return The enum object's name (either thimble, boot, racecar) in lowercase.(Look at the documentation for java enums to figure out how to get the enum value's name).
+	 */
 	public String toString()
 	{
 		return name().toLowerCase();
