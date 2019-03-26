e51f8f40941abfe131b0b0a53c417170997e683f
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-25 23:00:07 -0500

    First commit

---
+++ Todo.java
@@ -0,0 +1,35 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-25
+OU ID_______________: 113371387
+OU EMAIL____________: Kiley.m.peters-1@ou.edu
+
+Tasks:
+1. Complete BoardGame class.
+2. Complete Color class.
+3. Complete GamePiece class.
+4. Complete GamePieceAppearance class.
+5. Complete Location class.
+6. Complete Shape class.
+7. Create tests.
+
+
+
+Estimated Time (minutes):
+1. 120
+2. 60
+3. 60
+4. 60
+5. 60
+6. 60
+7. 120
+
+Actual Time (minutes):
+1. 300
+2. 60
+3. 60
+4. 60
+5. 60
+6. 60
+7. 300
+*/}
