c2ab03d666e58b9c23194699ae5ad34e82bd56e6
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-03-26 13:56:37 -0500

    Created javadoc

--- src/GamePieceAppearance.java
+++ src/GamePieceAppearance.java
@@ -1,19 +1,40 @@
-
+/**
+ * Class representing the game piece appearances.
+ * 
+ * @author Kiley
+ *
+ */
 public class GamePieceAppearance {
 
 	private Color color;
 	private Shape shape;
 	
+	/**
+	 * Stores the color and shape describing the appearance of the game piece.
+	 * 
+	 * @param color The game piece's color.
+	 * @param shape The game piece's shape.
+	 */
 	GamePieceAppearance(Color color, Shape shape)
 	{
 		this.color = color;
 		this.shape = shape;
 	}
 
+	/**
+	 * Gets the color of the game piece.
+	 * 
+	 * @return The game piece's color.
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Gets the shape of the game piece.
+	 * 
+	 * @return The game piece's shape.
+	 */
 	public Shape getShape() {
 		return shape;
 	}
